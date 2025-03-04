0e7f9ccf66ada10418c91ef272ba911a681b4152
Annamalai Raja <annamalai.raja@testunity.com>
2025-03-04 19:09:56 +0530

    Add meta description, update CSS styles, and improve content
    - Include meta description for better SEO.
    - Change font from Poppins to DM Sans.
    - Enhance sidebar and navbar styles for better visual appearance.
    - Update content in the skills, experience, and projects sections for clarity and relevance.
    - Add smooth scrolling behavior for anchor links.

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function toggleMenu() {
     const navLinks = document.getElementById("nav-links");
     navLinks.classList.toggle("active");
 }
+
+function toggleSidebar() {
+    document.querySelector(".sidebar").classList.toggle("active");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
